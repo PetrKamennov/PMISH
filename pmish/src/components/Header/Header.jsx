fd8e974f7dc36da60781b05ee81979cfabfda75f
pray312il <garanov_03@mail.ru>
2024-01-16 13:29:54 +0400

    adaptve update

--- pmish/src/components/Header/Header.jsx
+++ pmish/src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import './Header.css'
 import logoPISH from './img/logoPISH.png'
 import logoSamGMU from './img/logoSAMGMU.png'
 const Header = () => {
+	const MenuContext = createContext({
+		isMenuOpen: true,
+		toggleMenu: () => {},
+	  });
+	  
+	  const NavState = ({ children }) => {
+		const [isMenuOpen, toggleMenu] = useState(false);
+
+	  }
+		const { isMenuOpen, toggleMenuMode } = useContext(MenuContext);
+
+		const clickHandler = (e) => {
+			e.preventDefault()
+		};
 	return (
 		<>
 			<header className='Header'>
@@ -10,6 +24,11 @@ const Header = () => {
 					<img src={logoPISH} alt='PISH' />
 					<img src={logoSamGMU} alt='SamGMU' />
 				</div>
+				<a href="" onClick={clickHandler} className={`burger_button ${isMenuOpen ? 'active' : ""}`}>
+					<span className="button_bar"></span>
+					<span className="button_bar"></span>
+					<span className="button_bar"></span>
+				</a>
 				<nav className='Header__navblock'>
 					<ul>
 						<li>Образование</li>
